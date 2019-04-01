91775fada169a7618bd064239b0459cd19b7b612
silrait <thiarllisba@hotmail.com>
2019-04-01 02:37:55 -0300

    Product List at Home Page and Upvote Action. This is where the training ends.

--- product_hunt_clone/settings.py
+++ product_hunt_clone/settings.py
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/accounts/login'
--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.create, name='create')
+    path('create', views.create, name='create'),
+    path('<int:product_id>', views.detail, name='details'),
+    path('<int:product_id>/upvote', views.upvote, name='upvote')
 ]
